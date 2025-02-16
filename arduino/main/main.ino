8e57129849ee8d85c0fa9faeedb52197372ddd54
Garret Langlois <gaslanglois@gmail.com>
2025-02-16 17:46:52 -0500

    removed commenting

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -4,20 +4,15 @@ using namespace websockets;
 WebsocketsClient client;
 
 void setup() {
-  // Initialize serial and WiFi here
   Serial.begin(115200);
-  // Connect to WiFi...
-  
-  // Connect to the Flask WebSocket server
+
   client.connect("ws://192.168.x.x:5000/socket.io/?EIO=4&transport=websocket");
 }
 
 void loop() {
-  // Send fake sensor data periodically
   int fakeData = random(0, 100);
   client.send("data:" + String(fakeData));
   delay(1000);
-  
-  // Handle incoming messages if necessary
+
   client.poll();
 }
